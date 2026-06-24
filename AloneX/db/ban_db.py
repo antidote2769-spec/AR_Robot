71169c29f289e25660205ce407c43b190cce23b0
antidote2769-spec <antidote2769@gmail.com>
2026-06-24 12:20:27 +0530

    Update ban_db.py

--- AloneX/db/ban_db.py
+++ AloneX/db/ban_db.py
@@ -1,21 +1,20 @@
-from pymongo import MongoClient
-from AloneX import mongodb
+from AloneX import database
 
-blocked_users = mongodb.blocked_users
+blocked_users = database.blocked_users
 
-def ban_user(user_id):
-    blocked_users.update_one(
+async def ban_user(user_id):
+    await blocked_users.update_one(
         {"user_id": user_id},
         {"$set": {"user_id": user_id}},
         upsert=True
     )
 
-def unban_user(user_id):
-    blocked_users.delete_one(
+async def unban_user(user_id):
+    await blocked_users.delete_one(
         {"user_id": user_id}
     )
 
-def is_banned(user_id):
-    return blocked_users.find_one(
+async def is_banned(user_id):
+    return await blocked_users.find_one(
         {"user_id": user_id}
     )
