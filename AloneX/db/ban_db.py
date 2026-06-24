9f49591016d3b4dafa4733cd98027bd5b6fdc83a
antidote2769-spec <antidote2769@gmail.com>
2026-06-24 15:32:37 +0530

    Update ban_db.py

--- AloneX/db/ban_db.py
+++ AloneX/db/ban_db.py
@@ -19,3 +19,26 @@ async def is_banned(user_id):
         {"user_id": user_id}
     )
     return bool(user)
+    
+from AloneX import database
+
+blocked_users = database.blocked_users
+
+
+async def ban_user(user_id: int):
+    return await blocked_users.update_one(
+        {"user_id": user_id},
+        {"$set": {"user_id": user_id}},
+        upsert=True
+    )
+
+
+async def unban_user(user_id: int):
+    return await blocked_users.delete_one(
+        {"user_id": user_id}
+    )
+
+
+async def is_banned(user_id: int) -> bool:
+    data = await blocked_users.find_one({"user_id": user_id})
+    return data is not None
